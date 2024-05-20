c6b9c5d50a4218391e9ea9ccffcd2c2c00e4feee
Matic Ozebek <matic@amblyoplay.com>
2024-05-20 15:17:34 +0200

    Hosting setup

--- hosting/projects/app/src/environments/environment.ts
+++ hosting/projects/app/src/environments/environment.ts
@@ -1,8 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "--REPLACE--",
-    authDomain: "--ME--",
-    databaseURL: "----",
-    projectId: "----",
+    apiKey: "AIzaSyDxg_zsDWD1D3EEovH79_FW8vxX9LvVetg",
+    authDomain: "twlinch-records.firebaseapp.com",
+    databaseURL: "https://twlinch-records-default-rtdb.firebaseio.com",
+    projectId: "twlinch-records",
+    storageBucket: "twlinch-records.appspot.com",
+    messagingSenderId: "191415158619",
+    appId: "1:191415158619:web:490c0b69add3f717eafc7e",
+    measurementId: "G-JFFPG6677D",
   },
 };
